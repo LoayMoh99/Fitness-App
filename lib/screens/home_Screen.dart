e9757c3e454aeb173ad76cc380c6369217ac341a
Loay Mohamed <loaymohamed1999@gmail.com>
2020-07-15 22:49:12 +0200

    Future Builder Added

--- lib/screens/home_Screen.dart
+++ lib/screens/home_Screen.dart
@@ -15,22 +15,93 @@ class _HomeScreenState extends State<HomeScreen> {
   final String apiURL =
       "https://raw.githubusercontent.com/codeifitech/fitness-app/master/exercises.json";
 
-  ExcersiseHub excersiseHub;
-
-  void getExcersises() async {
+  bool noNetwork = false;
+  Future<ExcersiseHub> _getExcersises() async {
     var response = await http.get(apiURL);
     var bodyResponse = response.body;
     var decodedJson = jsonDecode(bodyResponse);
-    excersiseHub = ExcersiseHub.fromJson(decodedJson);
+    return ExcersiseHub.fromJson(decodedJson);
   }
 
-  @override
-  void initState() {
-    super.initState();
-
-    setState(() {
-      getExcersises();
-    });
+  Widget listExercises(ExcersiseHub excersiseHub) {
+    return ListView(
+      children: excersiseHub.exercises.map((e) {
+        return InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                return DetailScreen(
+                  exercises: e,
+                );
+              }),
+            );
+          },
+          child: Hero(
+            tag: e.id,
+            child: Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Stack(
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: CachedNetworkImage(
+                      imageUrl: e.thumbnail,
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 3,
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) => Image(
+                        image: AssetImage('assets/placeholder.jpg'),
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height / 3,
+                        fit: BoxFit.cover,
+                      ),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.center,
+                        colors: [
+                          Color(0xFF000000),
+                          Color(0x00000000),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 10, bottom: 10),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Align(
+                      child: Text(
+                        e.title,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      alignment: Alignment.bottomLeft,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      }).toList(),
+    );
   }
 
   @override
@@ -41,108 +112,35 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Text('Fitness App'),
         ),
       ),
-      body: (excersiseHub != null)
-          ? ListView(
-              children: excersiseHub.exercises.map((e) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) {
-                        return DetailScreen(
-                          exercises: e,
-                        );
-                      }),
-                    );
-                  },
-                  child: Hero(
-                    tag: e.id,
-                    child: Container(
-                      margin: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Stack(
-                        children: <Widget>[
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            // child: FadeInImage(
-                            //   width: MediaQuery.of(context).size.width,
-                            //   height: MediaQuery.of(context).size.height / 3,
-                            //   fit: BoxFit.cover,
-                            //   placeholder: AssetImage('assets/placeholder.jpg'),
-                            //   image: NetworkImage(e.thumbnail),
-                            // ),
-                            child: CachedNetworkImage(
-                              imageUrl: e.thumbnail,
-                              width: MediaQuery.of(context).size.width,
-                              height: MediaQuery.of(context).size.height / 3,
-                              fit: BoxFit.cover,
-                              placeholder: (context, url) => Image(
-                                image: AssetImage('assets/placeholder.jpg'),
-                                width: MediaQuery.of(context).size.width,
-                                height: MediaQuery.of(context).size.height / 3,
-                                fit: BoxFit.cover,
-                              ),
-                              errorWidget: (context, url, error) =>
-                                  Icon(Icons.error),
-                            ),
-                          ),
-                          Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height / 3,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              gradient: LinearGradient(
-                                begin: Alignment.bottomCenter,
-                                end: Alignment.center,
-                                colors: [
-                                  Color(0xFF000000),
-                                  Color(0x00000000),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(left: 10, bottom: 10),
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height / 3,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Align(
-                              child: Text(
-                                e.title,
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white),
-                              ),
-                              alignment: Alignment.bottomLeft,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              }).toList(),
-            )
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                LinearProgressIndicator(),
-                FlatButton(
-                    color: Colors.blue,
-                    onPressed: () {
-                      setState(() {
-                        getExcersises();
-                      });
-                    },
-                    child: Text('Refresh'))
-              ],
-            ),
+      body: FutureBuilder<ExcersiseHub>(
+        future: _getExcersises(),
+        builder: (context, snapshot) {
+          if (snapshot.error == true) {
+            return Center(
+              child: Text('Error'),
+            );
+          } else {
+            if (snapshot.connectionState == ConnectionState.done)
+              return listExercises(snapshot.data);
+            else if (snapshot.connectionState == ConnectionState.waiting)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            else
+              return Text('Unexpected');
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          setState(() {
+            _getExcersises();
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -8,33 +8,35 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  static const pageDecoration = const PageDecoration(
+    pageColor: Color(0xFF192A56),
+    bodyTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 20,
+    ),
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 36,
+    ),
+  );
   var pages = [
     PageViewModel(
-      title: "Title of first page",
-      body:
-          "Here you can write the description of the page, to explain someting...",
-      image: Center(child: Image.asset("assets/3.png", height: 175.0)),
-      decoration: const PageDecoration(
-        pageColor: Colors.blue,
-      ),
-    ),
-    PageViewModel(
-      title: "Title of first page",
-      body:
-          "Here you can write the description of the page, to explain someting...",
-      image: Center(child: Image.asset("assets/4.png", height: 175.0)),
-      decoration: const PageDecoration(
-        pageColor: Colors.blue,
-      ),
-    ),
-    PageViewModel(
-      title: "Title of first page",
-      body:
-          "Here you can write the description of the page, to explain someting...",
+      title: "Wanna Be Fit??",
+      body: 'Yesss',
       image: Center(child: Image.asset("assets/5.png", height: 175.0)),
-      decoration: const PageDecoration(
-        pageColor: Colors.blue,
-      ),
+      decoration: pageDecoration,
+    ),
+    PageViewModel(
+      title: "You CAN be what you Imagined!",
+      body: 'How then?!!',
+      image: Center(child: Image.asset("assets/4.png", height: 175.0)),
+      decoration: pageDecoration,
+    ),
+    PageViewModel(
+      title: "➡️ Let's Start ",
+      body: '..',
+      image: Center(child: Image.asset("assets/6.png", height: 175.0)),
+      decoration: pageDecoration,
     ),
   ];
   void navigate() {
@@ -59,14 +61,26 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           navigate();
         },
         showSkipButton: true,
-        skip: const Icon(Icons.skip_next),
-        next: const Icon(Icons.arrow_right),
-        done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+        skip: const Icon(
+          Icons.skip_next,
+          color: Colors.white,
+          size: 30,
+        ),
+        next: const Icon(
+          Icons.arrow_right,
+          color: Colors.white,
+          size: 40,
+        ),
+        done: const Text(
+          "Done",
+          style: TextStyle(
+              fontWeight: FontWeight.w600, color: Colors.white, fontSize: 20),
+        ),
         dotsDecorator: DotsDecorator(
             size: const Size.square(10.0),
             activeSize: const Size(20.0, 10.0),
             activeColor: Colors.deepOrange,
-            color: Colors.black26,
+            color: Colors.white60,
             spacing: const EdgeInsets.symmetric(horizontal: 3.0),
             activeShape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(25.0))),
